b6619f4d01e8f60de5acf93e7e55dc44c56b7242
elzzup <guild0105@gmail.com>
2015-12-29 17:54:08 +0900

    Install annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer          not null, primary key
+#  tid         :string
+#  screen_name :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class User < ActiveRecord::Base
   has_many :tweets, :dependent => :delete_all
 end
