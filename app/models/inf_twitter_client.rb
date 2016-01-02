f382a27583b097b9b104cb56fd63e755af3dc227
elzzup <guild0105@gmail.com>
2016-01-02 21:37:51 +0900

    InfTwitterClient token manage

--- app/models/inf_twitter_client.rb
+++ app/models/inf_twitter_client.rb
@@ -1,10 +1,54 @@
 class InfTwitterClient
   def initialize
     setup_token(Rails.application.secrets.twitter['tokens'])
-    # client =
+  end
+
+  def load_client
+  end
+
+  def check_clients
+    reload_client
+    # binding.pry
+    @client.perform
+  end
+
+  def reload_client
+    token = @next_token
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = token.consumer_key
+      config.consumer_secret     = token.consumer_secret
+      config.access_token        = token.access_key
+      config.access_token_secret = token.access_secret
+    end
   end
 
   def setup_token(tokens_s)
-    self.tokens = parse_token(tokens_s)
+    @tokens = parse_token(tokens_s)
+    @next_token = Enumerator.new do |y|
+      while true
+        @tokens.each do |token|
+          y << token
+        end
+      end
+    end
+  end
+
+  def parse_token(tokens_s)
+    tokens_s.split(':').each_slice(4).map do |ts|
+      TwitterToken.new(ts[0], ts[1], ts[2], ts[3])
+    end
+  end
+
+  def next_token
+    @next_token.next
+  end
+
+  class TwitterToken
+    def initialize(consumer_key, consumer_secret, token_key, token_secret)
+      @consumer_key = consumer_key
+      @consumer_secret = consumer_secret
+      @token_key = token_key
+      @token_secret = token_secret
+    end
   end
 end
--- lib/tasks/collector.rake
+++ lib/tasks/collector.rake
@@ -2,5 +2,6 @@ namespace :collector do
   desc 'debug'
   task :check_token => :environment do
     client = InfTwitterClient.new
+    client.check_clients
   end
 end
