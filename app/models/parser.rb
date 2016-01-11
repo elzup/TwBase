93ac8faf5e79c4f870095581ff409d9ed0c8a795
elzzup <guild0105@gmail.com>
2016-01-12 02:31:51 +0900

    Going Chunks join text

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -6,21 +6,30 @@ class Parser
 
   def where_go(text)
     tree = @parser.parse(text)
-    target_chunk = Parser.go_targets(tree)
+    Parser.go_targets(tree)
   end
 
   def self.go_targets(tree)
-    Parser.go_link_chunk(tree).each do |chunk|
+    go_chunk = Parser.go_chunk(tree)
+    Parser.linked_chunks(tree, go_chunk).each do |chunk|
       if Parser.target_chunk?(tree, chunk)
-        return chunk
+        return Parser.linked_chunks(tree, chunk).map do |target_link_chunk|
+          Parser.chunk_surface(tree, target_link_chunk)
+        end.join + Parser.chunk_head_surface(tree, chunk)
       end
     end
     return nil
   end
 
-  def self.go_link_chunk(tree)
+  def self.go_chunk(tree)
     (0...tree.chunk_size).map{ |i| tree.chunk(i) }.select do |chunk|
-      chunk.link >= 0 && Parser.go_chunk?(tree, tree.chunk(chunk.link))
+      Parser.go_chunk?(tree, chunk)
+    end[0]
+  end
+
+  def self.linked_chunks(tree, to_chunk)
+    (0...tree.chunk_size).map{ |i| tree.chunk(i) }.select do |chunk|
+      chunk.link >= 0 && tree.chunk(chunk.link).token_pos == to_chunk.token_pos
     end
   end
 
@@ -34,5 +43,17 @@ class Parser
 
   def self.enc(s)
     return s.force_encoding('UTF-8')
+  end
+
+  def self.chunk_surface(tree, chunk)
+    (chunk.token_pos...(chunk.token_pos + chunk.token_size)).map do |i|
+      Parser.enc(tree.token(i).surface)
+    end.join
+  end
+
+  def self.chunk_head_surface(tree, chunk)
+    (chunk.token_pos...(chunk.token_pos + chunk.func_pos)).map do |i|
+      Parser.enc(tree.token(i).surface)
+    end.join
   end
 end
